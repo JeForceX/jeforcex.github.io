6c8d1b57cd93fae43ca8dddbb1af0af64da183dc
Jason Wong <jeforcex@live.com>
2020-12-02 16:07:52 +0800

    Cumulative Build 2020.12.02

--- game/include/game.js
+++ game/include/game.js
@@ -4,21 +4,58 @@ Role-Playing Game
 JavaScript Engine
 ----------------- */
 
-const player = {
+/* Section: User Interface
+-------------------------- */
+
+// Message Dialog
+const ui_dialog = {
+  window: document.getElementById("ui_dialog_window"),
+  title: document.getElementById("ui_dialog_title").innerHTML,
+  content: document.getElementById("ui_dialog_content").innerHTML,
+  button: document.getElementById("ui_dialog_button")
+}
+
+// Set Dialog Position to Center of Screen
+function ui_dialog_refresh() {
+  ui_dialog.window.style.top = ((window.innerHeight - ui_dialog.window.clientHeight) * 0.5) + "px";
+  ui_dialog.window.style.left = ((window.innerWidth - ui_dialog.window.clientWidth) * 0.5) + "px";
+}
+
+/* Section: Player Character
+---------------------------- */
+
+const player = { // Player Character
   profile: {
     firstName: "",
     lastName: "",
     gender: "",
     race: "",
     class: "",
+    ideology:"",
     biography: ""
   },
-  ability: {
+  attribute_base: { // Base Attribute Score
     STR: 10, // Strength
     DEX: 10, // Dexterity
     CON: 10, // Constitution
     WIS: 10, // Wisdom
     INT: 10, // Intelligence
     CHA: 10  // Charisma
+  },
+  attribute_bonus: { // Bonus Attribute Score
+    STR: 0, // Strength
+    DEX: 0, // Dexterity
+    CON: 0, // Constitution
+    WIS: 0, // Wisdom
+    INT: 0, // Intelligence
+    CHA: 0  // Charisma
   }
 }
+
+/* Section: Character Creation
+------------------------------ */
+
+/* Section: System Boot
+----------------------- */
+
+ui_dialog_refresh(); // Set Dialog Position to Center of Screen
--- include/site.js
+++ include/site.js
@@ -11,20 +11,69 @@ JavaScript Engine
 const current_date = new Date();
 const current_year = current_date.getFullYear();
 const jfx_copyright_year = document.getElementById("jfx_copyright_year");
+jfx_copyright_year.innerHTML = current_year;
+
+/* Module Controller
+-------------------- */
+
+// Individual Modules
+const mod_forge = document.getElementById("forge");
+const mod_game = document.getElementById("game");
+const mod_guide = document.getElementById("guide");
+const mod_about = document.getElementById("about");
+const mod_privacy = document.getElementById("privacy");
+const mod_terms = document.getElementById("terms");
+
+// List of ALL Modules
+const mod_list = [
+  mod_forge,
+  mod_game,
+  mod_guide,
+  mod_about,
+  mod_privacy,
+  mod_terms
+]
+
+// Show ALL Modules
+function mod_list_show() {
+  for (i = 0; i < mod_list.length; i++) {
+    mod_list[i].style.display = "block";
+  }
+}
+
+// Hide ALL Modules
+function mod_list_hide() {
+  for (i = 0; i < mod_list.length; i++) {
+    mod_list[i].style.display = "none";
+  }
+}
 
 /* Navigation Controller
 -------------------- */
 
+function gotoHome() {
+  mod_list_show();
+  mod_privacy.style.display = "none";
+  mod_terms.style.display = "none";
+}
+
+function gotoGame() {
+  alert("Under Construction");
+}
+
 function gotoPrivacy() {
-  alert("Privacy Policy: Under Construction");
+  mod_list_hide();
+  mod_privacy.style.display = "block";
 }
 
 function gotoTerms() {
-  alert("Terms of Use: Under Construction");
+  mod_list_hide();
+  mod_terms.style.display = "block";
 }
 
 /* System Boot Controller
 ------------------------- */
 
 // Functions to be Executed during System Boot
-jfx_copyright_year.innerHTML = current_year;
+mod_privacy.style.display = "none";
+mod_terms.style.display = "none";
