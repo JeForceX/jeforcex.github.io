51acc621797de1e9fbafc1738794ca14bc387c1e
JeForceX <jeforcex@live.com>
2026-02-09 18:27:55 +0800

    New Artifacts #260209

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 /* -------------------------------------------------------------
-WHAT    : Application Launcher
-WHY     : Execute Mission Critical Function to launch the
-          Application
+WHAT : Application Launcher
+WHY  : Execute Mission Critical Function to launch the
+       Application
 ------------------------------------------------------------- */
 
+// Product Metadata
 const product = {
   name: 'JΞFØRCΞX GitHub Pages Site',
   version: '0.0.1'
 }
 
+// API Library
+const api = {
+  getYear: (id) => {
+    let target = document.getElementById(id);
+    let YYYY = new Date().getFullYear();
+    target.textContent = YYYY;
+  }
+}
+
+// Application Start-up Function Execution
+api.getYear('YYYY');
 console.log(`[System] SUCCESS: ${product.name} v${product.version} is operational.`);
